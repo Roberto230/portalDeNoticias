d061c7f0de2c7af1b54d8c36cf4f3a20a5593b42
Roberto Costa <robertocosta_96@outlook.com>
2022-06-30 22:33:55 -0300

    Desenvolvendo portal 1

--- index.js
+++ index.js
@@ -5,25 +5,31 @@ const path = require('path');
 
 const app = express();
 
-app.use(bodyParser.json() );
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.engine('html', require('ejs').renderFile);
-app.set('viw engine', 'htmo');
+app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname,'/views'));
+app.set('views', path.join(__dirname, '/pages'));
+
 
 app.get('/',(req,res)=>{
-    console.log(req.query);
-
+    
     if(req.query.busca == null){
-        res.send('home');
+        res.render('home',{});
     }else{
         res.send('Você buscou: '+req.query.busca);
     }
 
+  
+});
+
+
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug);
 })
 
 
